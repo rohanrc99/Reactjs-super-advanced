60f0a2c4c998ed28dde5fcceaaad30d142227d60
rohanrc99 <rohan98.kn@gmail.com>
2021-07-05 09:29:37 +0530

    basics of react

---
+++ src/1. components/1. functional/Greet.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Greet(){
+    return (
+        <React.Fragment>
+            <h2>Hello Rohan</h2>
+            <span>This is span</span>
+        </React.Fragment>
+    )
+}
+
+export default Greet;
---
+++ src/1. components/1. functional/Named.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export function Named(){
+    return <h1>This is Named Component</h1>
+}
